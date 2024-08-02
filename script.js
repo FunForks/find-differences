5a38840f441ef5b2bb61fa904cb5168990ca9682
James Newton <blackslate@lexogram.com>
2024-08-02 12:37:53 +0300

    Checkboxes, rollover divs and highlights in labels

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 /**
  * script.js
  */
+
+document.body.onclick = ({ target }) => {
+  console.log("target:", target);
+  const breadcrumbs = []
+  while (target) {
+    let classNames = (
+      Array.from(target.classList || [])
+    ).join(".")
+    const element = target.tagName + (classNames ? "."+classNames : "")
+    
+    breadcrumbs.push (element)
+    target = target.parentNode
+  }
+  console.log("breadcrumbs:", breadcrumbs);
+  
+}
